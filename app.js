e02884780eab82420fdfbf8a03ac4cd1263c8d1c
ericatsu <ekaatsu@st.ug.edu.gh>
2023-01-17 15:18:38 +0000

    page switch and error handling

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.write('My server created')
-    response.end()
+    if (request.url == '/') {
+        response.end('Server home here')
+    } 
+    else if (request.url == '/about') {
+        response.end('Server About here')
+    }
+    else {
+        response.end(`
+    <h1>Oops Can't be found</h1>
+    <a href='/'> home page </a>
+    `)
+    }
 })
 
 server.listen(5000);
