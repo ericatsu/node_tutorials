d9714ea1be135b8416b6a20b0234540c81c71b35
ericatsu <ekaatsu@st.ug.edu.gh>
2023-01-23 09:34:21 +0000

    learning async

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-console.log('First Loop');
-setTimeout(() => {
-    console.log('second loop');
-}, 0);
-console.log('third loop');
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  if (request.url === '/') {
+    response.end('This is the home page');
+  } else if (request.url === '/about') {
+    for (let i = 0; i < 1000; i++) {
+     for (let j = 0; j < 1000; j++) {
+         response.end(`${i} ${j}`)
+     }
+    }
+  } else {
+    response.end('error')
+  }
+})
+
+server.listen(5000, () => { 
+    console.log('Server is listen on port: 5000');
+})
