90ce438cce702cfb432a026a55653ca232fcb5fd
ericatsu <ekaatsu@st.ug.edu.gh>
2023-01-25 12:25:34 +0000

    using create stream

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const http = require('http')
 const fs = require('fs');
 
 http.createServer(function(req, res) {
-  const text = fs.readFileSync('./content/big.txt', 'utf8')
-  res.end(text);
+  const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
+  fileStream.on('open', ()=>{
+   fileStream.pipe(res)
+  })
+  fileStream.on('err',(err) => {
+    res.end(err)
+  })
 }).listen(5000)
 
 
